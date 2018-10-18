064e8b448f5e81fcf4b5aef463d3771b87a08bc5
davis-mwangi <daudimwash@gmail.com>
2018-10-18 13:34:20 +0300

    [Chore #161210981]add fecth all sale records endpoint

--- API-V1/tests/test_app.py
+++ API-V1/tests/test_app.py
@@ -139,3 +139,41 @@ def test_attendant_can_create_sale_record(client):
     response = post_json(client, '/api/v1/sales', sale_record, credentials)
     assert response.status_code == 201
     assert json_of_response(response) == {'message': 'New Sale record created'}
+
+
+def test_admin_cannot_create_sale_record(client):
+    """
+    Test admin not allowed to create a new sale record
+    """
+    sale_record = {
+
+        "sale_date": "17/10/2018",
+        "total_price": 2850,
+        "products_sold": [
+            {
+                "id": 1,
+                "product_name": "sugar",
+                "price_per_item": 200,
+                "items_sold": 10,
+                "total_amount": 2000
+            }
+
+        ]
+    }
+    credentials = base64.b64encode(b'david@gmail.com:david').decode('utf-8')
+    response = post_json(client, '/api/v1/sales', sale_record, credentials)
+    assert response.status_code == 401
+    assert json_of_response(response) == {'error': 'Not authorised to access'}
+
+
+def test_only_admin_can_access_sale_records(client):
+    """
+    Test only the admin/store owner can acces all the sale records
+    """
+    credentials = base64.b64encode(b'david@gmail.com:david').decode('utf-8')
+    response = get_json(client, '/api/v1/sales', credentials)
+    assert response.status_code == 200
+
+    auth = base64.b64encode(b'julius@gmail.com:julius').decode('utf-8')
+    response = get_json(client, '/api/v1/sales', auth)
+    assert response.status_code == 401
